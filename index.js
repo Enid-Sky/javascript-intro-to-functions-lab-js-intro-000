cfa424ef4643f3d62404ee9b53ea60fa2334919e
enid svymbersky <enid.bersky@gmail.com>
2019-11-15 02:53:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === lowercase){
+  if (string.toLowerCase() === true){
     return "I can't hear you!";
   }
 }
